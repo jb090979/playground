9960559768670e454cfcd998e1489969ff0c9f0d
Jens <jb090979@gmx.de>
2019-08-25 16:32:32 +0200

    HelloWorld

---
+++ HelloWorld/hello.cpp
@@ -0,0 +1,9 @@
+// A simple program that computes the square root of a number
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+int main (int argc, char *argv[])
+{ 
+  std::cout << "Hello World\n";
+  return 0;
+}
