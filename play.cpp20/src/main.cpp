6dc8b88245c7f02e5be1f35c5e220c385dd9a377
Jens Baumann <jb090979@gmx.de>
2022-09-29 20:17:44 +0200

    work with gcc

--- play.cpp20/src/main.cpp
+++ play.cpp20/src/main.cpp
@@ -6,7 +6,7 @@ import math;
 int main()
 {
     std::cout << "Play Modules:\n";
-    std:.cout << "add(2000,20) = " << add(2000,20) << "\n";
+    std::cout << "add(2000,20) = " << add(2000,20) << "\n";
 
     return 0;
 }
--- play.cpp20/src/math1.cpp
+++ play.cpp20/src/math1.cpp
@@ -22,5 +22,5 @@ export int add(int fir, int sec)
 
 export int getProduct(const std::vector<int>& vec)
 {
-    return std::accumulate(vec.begin(), vec.end(), 1, std:.multiplies<int>());
+    return std::accumulate(vec.begin(), vec.end(), 1, std::multiplies<int>());
 }
